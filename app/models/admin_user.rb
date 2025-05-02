26d41c467df45e3479adbfcbe8e3fe9a71ad87c9
Felipe Morais <felipemorais@Felipes-MacBook-Pro.local>
2025-05-02 18:43:35 -0300

    5 - modelo AdminUser e corrige erro de autenticação

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,7 @@
 class AdminUser < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[email current_sign_in_at sign_in_count created_at]
+  end
 end
